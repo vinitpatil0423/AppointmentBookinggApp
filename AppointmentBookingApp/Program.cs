6ffc855f9c1c8fd979ab708cbfde7bf173ff30d1
vinitpatil0423 <vinitpatil0423@gmail.com>
2026-09-13 23:10:57 +0530

    Update Program.cs

--- AppointmentBookingApp/Program.cs
+++ AppointmentBookingApp/Program.cs
@@ -18,6 +18,11 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     ));
 
 var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.Migrate();
+}
 
 if (!app.Environment.IsDevelopment())
 {
